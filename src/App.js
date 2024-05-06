efcf64b84289b93aef276ab1649530a58fba8a5a
TroddenSpade <troddenspade@gmail.com>
2024-05-06 20:25:19 +0330

    updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import * as React from 'react';
 import { CircularProgress, Button } from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+import Box from '@mui/material/Box';
+import { useTheme, ThemeProvider, createTheme } from '@mui/material/styles';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
 import { DataGrid } from '@mui/x-data-grid';
+import useMediaQuery from '@mui/material/useMediaQuery';
 import axios from 'axios';
 
 import './App.css';
 import logo from './logo.svg';
 
 
-function App() {
+function MainComponent() {
+  const theme = useTheme();
+  const colorMode = React.useContext(ColorModeContext);
 
   const [columns, setColumns] = React.useState([])
   const [rows, setRows] = React.useState([])
@@ -24,6 +32,7 @@ function App() {
         try_req = false
         return res
       });
+      console.log(tries + 1, " tries to fetch")
       tries++
     }
 
@@ -39,16 +48,17 @@ function App() {
         })
       })
       cols[cols.length-1]["renderCell"] = (params) => {
-        const file = new Blob([params[cols[cols.length-1].field]], {type: 'text/plain'});
+        const file = new Blob([atob(params.row[cols[cols.length-1].field])], {type: 'text/plain'});
         return (
           <Button
             variant="contained"
             size="small"
+            download={`${params.row[cols[0]["field"]]}.ovpn`}
+            target="_blank" 
+            rel="noreferrer" 
+            href={URL.createObjectURL(file)}
           >
-            <a download={`${params.row[cols[0]["field"]]}.ovpn`} target="_blank" rel="noreferrer" href={URL.createObjectURL(file)} style={{
-              textDecoration: "inherit",
-              color: "inherit",
-            }}>Download</a>
+            Download
           </Button>
         )
       }
@@ -62,14 +72,34 @@ function App() {
   }, [])
 
   return (
-    <div className="App">
+    <div className="App"
+    style={{
+      backgroundColor: theme.palette.bg,
+      color: theme.palette.color,
+    }}>
       <header className="App-header">
+        <Box
+          sx={{
+            display: 'flex',
+            width: '100%',
+            alignItems: 'center',
+            justifyContent: 'center',
+            borderRadius: 1,
+            p: 3,
+          }}
+        >
+          {theme.palette.mode} mode
+          <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode}>
+            {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+          </IconButton>
+        </Box>
       </header>
-      {
-      status===undefined ?
-        <CircularProgress />
-      :
-        <DataGrid
+      <div style={{padding: "20px"}}>
+        {
+          status===undefined ?
+          <CircularProgress />
+          :
+          <DataGrid
           rows={rows}
           columns={columns}
           initialState={{
@@ -78,10 +108,46 @@ function App() {
             },
           }}
           pageSizeOptions={[5, 10, 100]}
-        />
-      }
+          />
+        }
+      </div>
     </div>
   );
 }
 
-export default App;
+const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
+
+export default function App() {
+  
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const [mode, setMode] = React.useState(prefersDarkMode ? 'dark' : 'light');
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+          bg: mode === "dark" ? '#282c34' : "white",
+          color: mode === "dark" ? 'white' : "black",
+        },
+      }),
+    [mode],
+  );
+
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+      },
+    }),
+    [],
+  );
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <MainComponent />
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
